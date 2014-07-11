29a47176c929c4bf31fe9ad4ad9e8c323a0b8eb7
Hideyuki TAKEUCHI <chimerast@gmail.com>
2014-07-11 09:30:13 +0900

    並列実行が考慮されていなかったのを修正 LRUになっていなかったのを修正

--- karatachi-wicket/src/main/java/org/karatachi/wicket/core/NoSerializePageStore.java
+++ karatachi-wicket/src/main/java/org/karatachi/wicket/core/NoSerializePageStore.java
@@ -4,53 +4,82 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.wicket.page.IManageablePage;
 import org.apache.wicket.pageStore.IPageStore;
 
 public class NoSerializePageStore implements IPageStore {
-    final int pageNumbers;
+    private final int cacheSize;
 
-    private Map<String, Map<Integer, IManageablePage>> cache =
-            new HashMap<String, Map<Integer, IManageablePage>>();
+    private ConcurrentHashMap<String, Map<Integer, IManageablePage>> cache;
 
-    public NoSerializePageStore(int pageNumbers) {
-        this.pageNumbers = pageNumbers;
+    public NoSerializePageStore(int cacheSize) {
+        this.cacheSize = cacheSize;
+        this.cache =
+                new ConcurrentHashMap<String, Map<Integer, IManageablePage>>();
     }
 
+    @Override
     public void destroy() {
         cache = null;
     }
 
+    @Override
     public IManageablePage getPage(String sessionId, int pageId) {
         Map<Integer, IManageablePage> sessionCache =
                 getSessionCache(sessionId, false);
-        if(sessionCache != null) { 
+        if (sessionCache == null) {
+            return null;
+        }
+
+        synchronized (sessionCache) {
             return sessionCache.get(pageId);
         }
-        return null;
     }
 
+    @Override
     public void removePage(String sessionId, int pageId) {
-        getSessionCache(sessionId, false).remove(pageId);
+        Map<Integer, IManageablePage> sessionCache =
+                getSessionCache(sessionId, false);
+        if (sessionCache == null) {
+            return;
+        }
+
+        synchronized (sessionCache) {
+            sessionCache.remove(pageId);
+        }
     }
 
+    @Override
     public void storePage(String sessionId, IManageablePage page) {
-        getSessionCache(sessionId, true).put(page.getPageId(), page);
+        Map<Integer, IManageablePage> sessionCache =
+                getSessionCache(sessionId, true);
+        if (sessionCache == null) {
+            return;
+        }
+
+        synchronized (sessionCache) {
+            sessionCache.put(page.getPageId(), page);
+        }
     }
 
+    @Override
     public void unbind(String sessionId) {
         cache.remove(sessionId);
     }
 
+    @Override
     public Serializable prepareForSerialization(String sessionId, Object page) {
         return null;
     }
 
+    @Override
     public Object restoreAfterSerialization(Serializable serializable) {
         return null;
     }
 
+    @Override
     public IManageablePage convertToPage(Object page) {
         return (IManageablePage) page;
     }
@@ -60,17 +89,27 @@ public class NoSerializePageStore implements IPageStore {
         Map<Integer, IManageablePage> sessionCache = cache.get(sessionId);
         if (sessionCache == null) {
             if (create) {
-                sessionCache = new LinkedHashMap<Integer, IManageablePage>(pageNumbers) {
-                    private static final long serialVersionUID = 1L;
-
-                    @Override
-                    protected boolean removeEldestEntry(Map.Entry<Integer, IManageablePage> eldest) {
-                        return size() > pageNumbers;
-                    }
-                };
-                cache.put(sessionId, sessionCache);
+                HashMap<Integer, IManageablePage> newCache = newSessionCache();
+                sessionCache = cache.putIfAbsent(sessionId, newCache);
+                if (sessionCache == null) {
+                    sessionCache = newCache;
+                }
             }
         }
         return sessionCache;
+    }
+
+    private LinkedHashMap<Integer, IManageablePage> newSessionCache() {
+        // LRU Cache
+        return new LinkedHashMap<Integer, IManageablePage>(cacheSize, 0.75f,
+                true) {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected boolean removeEldestEntry(
+                    Map.Entry<Integer, IManageablePage> eldest) {
+                return size() > cacheSize;
+            }
+        };
     }
 }
